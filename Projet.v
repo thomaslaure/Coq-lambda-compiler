e96d0df628795fa32418cdbdad7e3f7e96e31cd7
thomaslaure <39703028+thomaslaure@users.noreply.github.com>
2021-05-31 17:26:03 +0200

    Final commit

--- Projet.v
+++ Projet.v
@@ -78,6 +78,13 @@ end.
 Definition subst (t : term) (i : nat) (u : term) := subst_par t i 0 u.
 
 
+Lemma null_shift : forall u : term, forall l : nat, shift u l 0 = u.
+Proof.
+  induction u. intros. simpl. case_eq (leb l n). rewrite Nat.add_0_r. trivial.
+  trivial. intros. simpl. f_equal. apply IHu. intros. simpl. f_equal. apply IHu1.
+  apply IHu2.
+Qed.
+
 (*preuves du résultat de subst_var*)
 Lemma subst_var_eq : forall i : nat, forall j : nat, forall p : nat, forall u : term, 
   forall v : term,
@@ -389,13 +396,20 @@ end.
 
 (*prédicat u -> v*)
 Fixpoint reducts (u : term) (v : term) : Prop :=
-  match u,v with
-  | app u1 u2, app v1 v2 => (redexes u1 u2 v) \/ 
+  match u with
+  | app u1 u2 => (redexes u1 u2 v) \/ 
+                  (match v with 
+                  | app v1 v2 =>
                   ((reducts u1 v1)/\(u2=v2))\/((u1=v1)/\(reducts u2 v2))
                 (*soit u_1 u_2 est le rédex contracté, soit la réduction se fait d'un côté de l'application*)
-  | app u1 u2, _ => redexes u1 u2 v (*nécessairement u_1 u_2 doit être le rédex contracté*)
-  | lambda u1, lambda v1 => reducts u1 v1 (*deux lambda-abstractions se réduisent l'une en l'autre*)
-  | _, _ => False
+                  | _ => False 
+                  end )(*nécessairement u_1 u_2 doit être le rédex contracté*)
+  | lambda u1 => match v with 
+              | lambda v1 =>  reducts u1 v1 (*deux lambda-abstractions se réduisent l'une en l'autre*)
+              | _  => False 
+                end
+  | _ => False 
+  
 end.
 
 
@@ -530,7 +544,7 @@ end.
 Fixpoint tau_acc (u : term ) (s : stack) : term :=
   match s with 
   | nillstack => u
-  | const c e q => tau_acc (mult_subst (tau_code c) 0 (tau_env e)) q
+  | const c e q => tau_acc (app u (mult_subst (tau_code c) 0 (tau_env e))) q
 end.
 
 Definition tau (s :state) : term :=
@@ -583,7 +597,7 @@ Definition correct_o (s : option state) : Prop :=
   | Some t => correct t
 end.
 
-
+(* une transition à partir d'un état correct mène à None où à un état correct*)
 Lemma correct_trans : forall s1 : state,
    correct s1 -> correct_o (semantics s1).
 Proof.
@@ -600,21 +614,358 @@ Qed.
   
 
 (*4*)
-Definition tau_o (s : option state) : option term := 
-  match s with 
-  | None => None 
-  | Some u => Some (tau u)
+(*On va faire une série de lemmes intermédiaires pour aboutir au résultat*)
+
+Fixpoint length_l (l : list term) : nat :=  
+  match l with 
+  | nil => 0
+  | cons u q => S (length_l q)
 end.
 
-
-Definition reducts_o (t : term) (u : option term) :=
-  match u with
-  | None => True
-  | Some v => reducts t v
-end.
-
-
-Lemma correct_red : forall s : state, correct s -> reducts_o (tau s) (tau_o (semantics s)).
+(*prouve que si une variable est d'indice plus petit que la longueur de la liste, alors 
+  décrémenter  la variable et l'indice de départ mène à la même chose, ceci car la 
+  substitution parallèle sélectionne le même terme de la liste*)
+Lemma var_dec_n : forall l : list term, forall n : nat, forall i : nat, n < length_l l -> 
+  mult_subst (var (n+ S i)) (S i) l = mult_subst (var (n+i)) i l. 
 Proof.
-  intros. induction s. induction c. induction e. induction n. simpl. trivial.
-  simpl. trivial. induction n. simpl. induction s. simpl.
+  induction l. intros. exfalso. apply (lt_n_0 n). apply H.
+  induction n. intros. apply (eq_ind (shift a 0 0)). apply int_2. trivial.
+  apply eq_sym. apply int_2. trivial.
+  intros. apply (eq_ind (mult_subst (var (S n + i)) (S i) l)).
+  apply (eq_ind (mult_subst (var (S n + S i)) (S (S i)) l)).
+  apply int_3. apply (plus_lt_compat_r 0 (S n) (S i)). apply lt_0_Sn.
+  rewrite (plus_Snm_nSm). rewrite (plus_Snm_nSm n i). apply (IHl n (S i)).
+  apply lt_S_n. apply H.
+  apply eq_sym. apply int_3. apply (plus_lt_compat_r 0 (S n) i). apply lt_0_Sn.
+Qed.
+
+(*prouve que sous la même hypothèse, on peut décrémenter la variable et supprimer le 
+  premier terme de la liste*)
+Lemma var_subst_n : forall l : list term, forall n : nat, forall u : term, 
+  n < length_l l -> mult_subst (var (S n)) 0 (cons u l) = mult_subst (var n) 0 l.
+Proof.
+  intros. apply (eq_ind (mult_subst (var (S n)) 1 l)). apply int_3. apply lt_0_Sn.
+  rewrite <- (Nat.add_0_r n). rewrite <- Nat.add_succ_r. apply var_dec_n. trivial.
+Qed.
+
+(*c'est évident mais utile, la longueur de la liste des termes tau_env e est la même 
+  que la longueur de e*)
+Lemma equals_lenghts_e : forall e : env, length_e e = length_l (tau_env e).
+Proof.
+  induction e. simpl. trivial. simpl. f_equal. apply IHe2.
+Qed.
+
+(*on peut maintenant prouver la propriété voulue pour les 3 transitions où il ne se passe 
+  rien : pour access n, access 0 et push, les tau de chaque côté des transitions coïncident*)
+Lemma correct_red_Sn : forall n : nat, forall e : env, forall c0 : code, forall e0 :env, forall s : stack,
+  correct (stat (access (S n)) (couple c0 e0 e) s) -> 
+    tau (stat (access (S n)) (couple c0 e0 e) s) = tau (stat (access n)  e s).
+Proof.
+  induction s. simpl. intros. apply var_subst_n. apply lt_S_n. rewrite <- equals_lenghts_e. 
+  apply H. intros. simpl. f_equal. f_equal. apply var_subst_n. rewrite <- equals_lenghts_e. 
+  apply lt_S_n. apply H.
+Qed.
+
+
+Lemma correct_red_0 : forall e : env, forall c0 : code, forall e0 :env, forall s : stack,
+    tau (stat (access 0) (couple c0 e0 e) s) = tau (stat c0 e0 s).
+Proof.
+  induction s. intros. simpl. apply (eq_ind (shift (mult_subst (tau_code c0) 0 (tau_env e0)) 0 0)).
+  apply int_2. trivial. apply null_shift.
+  intros. simpl. f_equal. f_equal. apply (eq_ind (shift (mult_subst (tau_code c0) 0 (tau_env e0)) 0 0)).
+  apply int_2. trivial. apply null_shift.
+Qed.
+
+
+Lemma correct_red_push : forall c : code, forall c' : code, forall e : env, forall s : stack,
+  tau (stat (push c' c) e s) = tau (stat c e (const c' e s)).
+Proof.
+  induction s. intros. simpl. trivial. simpl. f_equal.
+Qed.
+
+
+(*pour grab, il va falloir faire un peu plus de travail*)
+
+(*corollaire de int_2 (avec mult_subst_var) mais quand t est directement la variable var i*)
+Lemma int_2_prime : forall i : nat, forall u : term, forall l : list term, 
+  mult_subst (var i) i (cons u l) = u.
+Proof.
+  intros. apply (eq_ind (mult_subst_var i i 0 (cons u l) (var i))).
+  simpl. trivial. apply (eq_ind (shift u 0 0)). apply int_2. trivial. apply null_shift.
+Qed. 
+
+
+(*prouve que si un terme vérifie C i, alors shifter ses variables d'indice supérieur 
+  à i ne change rien (car ces variables n'existent pas)*)
+Lemma shift_simpl : forall p : nat, forall t : term, forall i : nat, C i t -> 
+        shift t i p = t.
+Proof.
+  induction t. intros. simpl. case_eq (leb i n).  intros. exfalso. apply (le_not_lt i n).
+  apply (leb_complete i n). trivial. apply H. intros. trivial. intros. simpl. 
+  f_equal. apply IHt. apply H. intros. simpl. f_equal. apply IHt1. apply H. apply IHt2. apply H.
+Qed. 
+
+(*prouve que si une liste ne contient que des termes clos, alors la substitution parallèle
+  avec un shift quelconque équivaut à le faire avec un shift nul (car on ne shifte que les termes de la liste,
+  et puisqu'ils sont clos cela ne change rien d'après le lemme précédent)*)
+Lemma mult_shift_simpl : forall l : list term, forall t : term, forall i : nat, forall p : nat, 
+  C_l 0 l -> mult_subst_par t i p l = mult_subst_par t i 0 l.
+Proof.
+  induction l. intros. apply (eq_ind t). apply m_s_p_nil. apply eq_sym. apply m_s_p_nil.
+  induction t. intros. elim (le_or_lt n i). intros. elim (le_lt_or_eq n i). intros.
+  apply (eq_ind (var n)). apply int_1.  trivial. apply eq_sym. apply int_1. trivial.
+  intros. rewrite H1.  apply (eq_ind a). apply (eq_ind (shift a 0 p)).
+  apply int_2. trivial. apply shift_simpl. apply H. apply eq_sym. apply (eq_ind (shift a 0 0)).
+  apply int_2. trivial. apply null_shift. trivial. intros. simpl. 
+  rewrite (int_3 (var n) l a i n p). rewrite (int_3 (var n) l a i n 0). apply (IHl (var n) (S i) p). apply H.
+  trivial. trivial. intros. simpl. f_equal. apply (eq_ind (mult_subst_par t (S i) 0 (cons a l))).
+   apply (IHt (S i) (S p)). trivial. apply eq_sym. apply (IHt (S i) 1). trivial.
+  intros. simpl. f_equal. apply IHt1. trivial. apply IHt2. trivial.
+Qed.
+
+(*corollaires de int_1 et int_3 quand t est var j, évidents mais coq refuse d'appliquer 
+  directement int_1 et int_3*)
+Lemma int_1_prime : forall i : nat, forall j : nat, forall l : list term, 
+  j < i -> mult_subst (var j) i l = var j.
+Proof.
+  intros. simpl. apply int_1. trivial.
+Qed.
+
+Lemma int_3_prime : forall i : nat, forall j : nat, forall l : list term, forall u : term, 
+  j>i -> mult_subst (var j) i (cons u l) = mult_subst (var j) (S i) l.
+Proof.
+  intros. apply int_3. trivial.
+Qed.
+
+(*prouve que si un terme est le shifté de 0 rangs d'un élément de l, alors il appartient à l*)
+Lemma null_shift_l : forall l : list term, forall t : term, belongs_shift t l 0 0 -> belongs t l.
+Proof.
+  induction l. intros. simpl. apply H. intros. elim H. intros. simpl. left.
+  rewrite <- (null_shift a 0). trivial. intros. simpl. right. apply IHl. trivial.
+Qed.
+
+(*dernier corollaire d'un int_i*)
+Lemma int_0_prime : forall i : nat, forall j : nat, forall l : list term, 
+  belongs (mult_subst (var j) i l) l \/ mult_subst (var j) i l = var j.
+Proof.
+  intros. elim (int_0 (var j) l i j 0). intros. left. apply null_shift_l. apply H.
+  intros. right. apply H.
+Qed.
+
+(*dans le même esprit que les int_i, prouve dans quel cas on sélectionne un élément
+  de la liste en le shiftant, puis on écrit encore son corollaire*)
+Lemma int_4 : forall v : term, forall l : list term, forall i : nat, forall j : nat, 
+  forall p : nat, i<= j -> j < (i + (length_l l)) -> belongs_shift (mult_subst_var i j p l v) l 0 p.
+Proof.
+  induction l. intros. exfalso. apply (le_not_lt i j). apply H. rewrite <- (Nat.add_0_r i). apply H0.
+  intros. elim (le_lt_or_eq i j). intros. rewrite int_3. simpl. right. apply (IHl (S i) j p).  apply H1. 
+  rewrite plus_Snm_nSm. apply H0. apply H1. intros. simpl. left. apply int_2. apply eq_sym. trivial. trivial.
+Qed.
+
+Lemma int_4_prime : forall i : nat, forall j : nat, forall l :list term, 
+   i<= j -> j < (i + (length_l l)) -> belongs (mult_subst (var j) i l) l.
+Proof.
+  intros. apply null_shift_l. apply int_4. trivial. trivial.
+Qed.
+
+(*si t vérifie C (i + len l) et qu'on remplace toutes les variables d'indice entre i et 
+  i+len l par des termes clos, alors le résultat vérifie C i*)
+Lemma by_closed : forall t : term, forall l : list term, forall i : nat,
+    C_l 0 l -> C (i+ (length_l l)) t -> C i (mult_subst t i l).
+Proof.
+  induction t. intros. elim (le_or_lt i n). intros. apply corollary_C. 
+  apply (C_recip (mult_subst (var n) i l) l 0). apply int_4_prime. trivial. trivial. trivial.
+  intros. rewrite int_1_prime. apply H1. trivial.
+  intros. simpl. rewrite mult_shift_simpl. apply IHt. trivial. apply H0. trivial.
+  intros. simpl. split. apply IHt1. trivial. apply H0. apply IHt2. trivial. apply H0.
+Qed. 
+    
+(*propriété fondamentale pour la suite : tau_env e ne renvoie que des termes clos*)
+Lemma closed_env : forall e : env, correct_e e -> C_l 0 (tau_env e).
+Proof.
+  induction e. intros. simpl. trivial.
+  intros. simpl. split. apply by_closed. apply IHe1. apply H. rewrite Nat.add_0_l.
+  rewrite <- equals_lenghts_e. apply H. apply IHe2. apply H.
+Qed.
+
+(*on peut maintenant prouver que pour un grab, si s est vide, alors on fait exactement
+  la réduction d'un rédex*)
+Lemma redexes_grab : forall c : code, forall e : env, forall c0 : code, forall e0 : env, forall s : stack, 
+  correct (stat (grab c) e (const c0 e0 s)) -> 
+  redexes (mult_subst (tau_code (grab c)) 0 (tau_env e)) (mult_subst (tau_code c0) 0 (tau_env e0)) 
+      (mult_subst (tau_code c) 0 (cons (mult_subst (tau_code c0) 0 (tau_env e0)) (tau_env e))).
+Proof.
+  intros. simpl. rewrite mult_shift_simpl. apply mult_subst_succ. apply closed_env. apply H.
+  apply closed_env. apply H.
+Qed.
+
+(*sert pour l'induction à cause de l'accumulateur, car on fait une réduction tout en bas à gauche*)
+Lemma red_trans_acc : forall s : stack, forall t1 : term, forall t2 : term, 
+  reducts t1 t2 -> reducts (tau_acc t1 s) (tau_acc t2 s).
+Proof.
+  induction s. intros. apply H.
+  intros. simpl. apply IHs. simpl. right. left. split. trivial. trivial.
+Qed.
+  
+
+(*on démontre enfin que la transition du grab fait une beta-réduction*)
+Lemma correct_red_grab : forall c : code, forall e : env, forall c0 : code,
+ forall e0 : env, forall s : stack, 
+  correct (stat (grab c) e (const c0 e0 s)) -> reducts (tau (stat (grab c) e (const c0 e0 s)))
+        (tau (stat c (couple c0 e0 e) s)).
+Proof.
+  intros. simpl. apply red_trans_acc. simpl. left. apply (redexes_grab c e c0 e0 s). apply H.
+Qed.
+
+(*prédicat : si s2 existe, alors soit les deux états sont égaux, soit ils se beta-réduisent*)
+Definition red_equals_o (s1 : state) (s2 : option state) : Prop :=
+  match s2 with
+  | None => True
+  | Some s => reducts (tau s1) (tau s) \/ tau s1 = tau s
+end.
+
+
+(*on prouve enfin qu'une transition à partir d'un état correct mène soit à None,
+  soit à un état qui est modulo tau identique ou un réduit*)
+Lemma correct_red : forall s : state, correct s -> red_equals_o s (semantics s).
+Proof.
+  intros. induction s. induction c. induction e. simpl. induction n ; simpl ; trivial.
+  induction n. simpl. right. apply correct_red_0. simpl. right. apply correct_red_Sn. apply H.
+  induction s. simpl. trivial. simpl. left. apply correct_red_grab. apply H.
+  simpl.  right.  apply correct_red_push.
+Qed.
+
+
+(*1.5*)
+(*on a travaillé avec les options pour que coq fasse les calculs plus facilement,
+  on va maintenant les éliminer et travailler avec des prédicats de réduction*)
+Definition reducts_sem (s1 : state) (s2 : state) : Prop :=
+  semantics s1 = Some s2.
+
+Lemma elim_option : forall s : state, correct_o (Some s) -> correct s.
+Proof.
+  intros. apply H.
+Qed.
+
+Lemma correct_trans_1 : forall s1 : state, forall s2 : state, correct s1 -> reducts_sem s1 s2 ->
+  correct s2.
+Proof.
+  intros. apply elim_option. rewrite <- H0. apply correct_trans. trivial.
+Qed.
+
+Lemma elim_option_bis : forall s1 : state, forall s2 : state, red_equals_o s1 (Some s2) -> (reducts (tau s1) (tau s2)
+    \/ tau s1 = tau s2).
+Proof.
+  intros. apply H. 
+Qed.
+
+Lemma correct_red_1 : forall s1 : state, forall s2 : state, correct s1 -> reducts_sem s1 s2 -> 
+  (reducts (tau s1) (tau s2) \/ tau s1 = tau s2).
+Proof.
+  intros. apply elim_option_bis. rewrite <- H0. apply correct_red. trivial.
+Qed.
+
+(*l'état est bloqué*)
+Definition blocked (s : state) : Prop := semantics s = None.
+(* on définit la réduction en n étapes par la machine et on passe par induction les 
+  propriétés précédentes*)
+Fixpoint reducts_sem_star_n (n : nat) (s1 : state) (s2 : state) : Prop :=
+  match n with 
+  | 0 => s1 = s2 
+  | S m => exists s : state, reducts_sem s1 s /\ reducts_sem_star_n m s s2
+end.
+
+Definition reducts_sem_star (s1 : state) (s2 : state) : Prop :=
+  exists n : nat, reducts_sem_star_n n s1 s2.
+
+
+Lemma correct_red_star_n : forall n : nat, forall s1 : state, forall s2 : state, correct s1 ->
+  reducts_sem_star_n n s1 s2 -> (correct s2 /\ reducts_star (tau s1) (tau s2)).
+Proof. 
+  induction n. intros. elim H0. split. trivial. apply reflex.
+  intros. elim H0. intros. split. apply (IHn x s2). apply (correct_trans_1 s1 x). 
+  trivial. apply H1. apply H1. elim (IHn x s2). intros. elim H3. intros.
+  elim (correct_red_1 s1 x). intros.
+  exists (S x0). exists (tau x). split. trivial. trivial.
+  intros. rewrite H5. trivial. trivial. apply H1. apply (correct_trans_1 s1 x). trivial. apply H1. apply H1.
+Qed.
+
+Lemma correct_red_star : forall s1 : state, forall s2 : state, correct s1 -> reducts_sem_star s1 s2 ->
+  (correct s2 /\ reducts_star (tau s1) (tau s2)).
+Proof.
+  intros. elim H0. intros. apply (correct_red_star_n x s1 s2). trivial. trivial.
+Qed.
+
+(*concaténation à droites des réductions, car la définition est à gauche*)
+Lemma right_trans : forall n : nat, forall t1 : term, forall t2 : term, forall t3 : term,
+  reducts_star_n t1 t2 n -> reducts t2 t3 -> reducts_star_n t1 t3 (S n).
+Proof.
+  induction n. intros. rewrite H. exists t3. split. trivial. simpl. trivial.
+  intros. elim H. intros. exists x. split. apply H1. apply (IHn x t2 t3). apply H1. trivial.
+Qed.
+
+Lemma right_trans_sem : forall n : nat, forall s1 : state, forall s2 : state, forall s3 : state, 
+  reducts_sem_star_n n s1 s2 -> reducts_sem s2 s3 -> reducts_sem_star_n (S n) s1 s3.
+Proof.
+  induction n. intros. rewrite H. exists s3. split. trivial. simpl. trivial.
+  intros. elim H. intros. exists x. split. apply H1. apply (IHn x s2 s3). apply H1. trivial.
+Qed.
+
+Lemma complete_option : forall o : option state, o = None \/ (exists s : state, o = Some s).
+Proof.
+  induction o. right.  exists a. trivial. left. trivial.
+Qed.
+
+(*tout état est soit bloquant, soit suivi d'une transition*)
+Lemma complete_trans : forall s : state, blocked s \/ (exists s1 : state, reducts_sem s s1).
+Proof.
+  intros. elim (complete_option (semantics s)). intros. left. apply H.
+  intros. right. elim H. intros. exists x. apply H0.
+Qed.
+
+(*pour tout n, soit on atteint un état bloquant en moins de n étapes, soit on peut continuer
+  au moins une étape de plus*)
+Lemma successor_n : forall s : state, forall n : nat, (exists p : nat, exists s1 : state,
+    p<= n /\ reducts_sem_star_n p s s1 /\ blocked s1) \/ (exists s1 : state, exists s2 : state,
+    reducts_sem_star_n n s s1 /\ reducts_sem s1 s2).
+Proof.
+  induction n. intros. elim (complete_trans s). intros. left. exists 0. exists s.
+  split. apply (le_refl 0). split. simpl. trivial. trivial. intros. right. exists s. elim H. intros. exists x.
+  split. simpl. trivial. trivial.
+  intros. elim IHn. intros. elim H. intros. elim H0. intros. left. exists x. exists x0.
+  split. apply (le_trans x n (S n)). apply H1. apply le_n_Sn. apply H1.
+  intros. elim H. intros. elim H0. intros. elim (complete_trans x0).
+  intros. left. exists (S n). exists x0. split. trivial. split. apply (right_trans_sem n s x x0).
+  apply H1. apply H1. trivial. intros. right. elim H2. intros. exists x0. exists x1.
+  split. apply (right_trans_sem n s x x0). apply H1. apply H1. trivial.
+Qed.
+
+(* on termine par prouver que l'état (compile t, [],[]) est correct et correspond bien à t via tau*) 
+Definition init_stat (t : term) : state := stat (compile t) nillenv nillstack.
+
+Lemma correct_init : forall t : term, closed t -> 
+    correct (init_stat t).
+Proof.
+  intros. simpl. split. trivial. split. trivial. rewrite compose. apply H.
+Qed.
+
+Lemma compose_stat : forall t : term, tau (init_stat t) = t.
+Proof.
+  intros. simpl. rewrite compose. apply mult_subst_nil.
+Qed.
+
+
+(* et on arrive enfin au théorème final de correction de la compilation*)
+
+Theorem correct_compilation : forall t : term, forall n : nat, closed t ->
+  exists s : state, reducts_star t (tau s) /\ ((blocked s /\ reducts_sem_star (init_stat t) s)
+    \/ reducts_sem_star_n n (init_stat t) s).
+Proof.
+  intros. elim (successor_n (init_stat t) n). intros. elim H0. intros. elim H1. intros.
+  exists x0. split. rewrite <- (compose_stat t). apply correct_red_star. apply correct_init. trivial.
+  exists x. apply H2. left. split. apply H2. exists x. apply H2.
+  intros. elim H0. intros. exists x. split. rewrite <- (compose_stat t). apply correct_red_star. apply correct_init.
+  trivial. exists n. elim H1. intros. apply H2. right. elim H1. intros. apply H2.
+Qed.
+
+
